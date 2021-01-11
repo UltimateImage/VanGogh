7014a69c1b2dbc62c5854eb16113f7297b2f5e4a
odysa <piensengxv@gmail.com>
2021-01-11 14:53:47 +0800

    feat: setup router and middlewares

---
+++ router/middlewares/header.go
@@ -0,0 +1,14 @@
+package middlewares
+
+import "github.com/gin-gonic/gin"
+
+//Secure setup security settings
+func Secure(c *gin.Context) {
+	c.Header("Access-Control-Allow-Origin", "*")
+	c.Header("X-Frame-Options", "DENY")
+	c.Header("X-Content-Type-Options", "nosniff")
+	c.Header("X-XSS-Protection", "1; mode=block")
+	if c.Request.TLS != nil {
+		c.Header("Strict-Transport-Security", "max-age=31536000")
+	}
+}
--- router/router.go
+++ router/router.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"VanGogh/handler/sc"
+	"VanGogh/router/middlewares"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -10,7 +11,7 @@ import (
 //Load default router setting
 func Load(g *gin.Engine, handlers ...gin.HandlerFunc) *gin.Engine {
 	g.Use(gin.Recovery())
-
+	g.Use(middlewares.Secure)
 	g.Use(handlers...)
 	g.NoRoute(func(c *gin.Context) {
 		c.String(http.StatusNotFound, "Resource not found")
