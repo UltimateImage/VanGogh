5505887d6ce442a204a05788579effbc730f393b
odysa <piensengxv@gmail.com>
2021-01-15 19:27:51 +0800

    feat: convert img to ascii

--- handler/convert/ascii.go
+++ handler/convert/ascii.go
@@ -1,7 +1,22 @@
 package convert
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/UltimateImage/VanGogh/pkg/errs"
+	"github.com/UltimateImage/VanGogh/utils"
+	"github.com/gin-gonic/gin"
+	"github.com/qeesung/image2ascii/convert"
+)
 
 func AsciiHandler(c *gin.Context) {
-
+	img, err := utils.ReadImageFromCtx(c)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": errs.NewError(errs.INVALIDIMAGE, "Invalid Image", err)})
+		return
+	}
+	convertOptions := convert.DefaultOptions
+	converter := convert.NewImageConverter()
+	res := converter.Image2ASCIIString(*img, &convertOptions)
+	c.JSON(http.StatusOK, *errs.OK(res))
 }
--- pkg/errs/code.go
+++ pkg/errs/code.go
@@ -1,4 +1,4 @@
-package errors
+package errs
 
 const (
 	//INVALIDIMAGE code
